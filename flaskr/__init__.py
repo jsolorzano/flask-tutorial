da3042adb8f628c1ca4b34ae8bcdb0d8bb64f695
José Solorzano <solorzano202009@gmail.com>
2019-06-05 16:07:24 -0400

    Inicio del tutorial de flask.

---
+++ flaskr/__init__.py
@@ -0,0 +1,31 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+	# Crea y configura la aplicación
+	app = Flask(__name__, instance_relative_config=True)
+	app.config.from_mapping(
+		SECRET_KEY='dev',
+		DATABASE=os.path.join(app.instance_path, 'flask.sqlite'),
+	)
+
+	if test_config is None:
+		# Cargamos la instancia config, si existe, entonces no testeamos
+		app.config.from_pyfile('config.py', silent=True)
+	else:
+		# Cargamos la configuración de prueba si se pasa
+		app.config.from_mapping(test_config)
+
+	# Nos aseguramos de que existe la carpeta de la instancia
+	try:
+		os.makedirs(app.instance_path)
+	except OSError:
+		pass
+
+	# Una página simple que dice Hola Mundo
+	@app.route('/hello')
+	def hello():
+		return 'Hello, World!'
+
+	return app	

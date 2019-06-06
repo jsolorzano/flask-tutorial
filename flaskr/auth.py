9f2d16fd212343f38deea44e0ceae8c25a6e9eb7
José Solorzano <solorzano202009@gmail.com>
2019-06-06 18:06:41 -0400

    Funciones de gestión inicial de publicaciones terminadas.

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -76,7 +76,7 @@ def load_logged_in_user():
 @bp.route('/logout')
 def logout():
 	session.clear()
-	return redirect(url_for(index))
+	return redirect(url_for('index'))
 
 def login_required(view):
 	@functools.wraps(view)
